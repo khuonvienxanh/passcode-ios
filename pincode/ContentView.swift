e2284fe38828c523ae0c6559ae14fc0e8b650c20
ChuyenDo <hanhchuyen6791@yahoo.com>
2020-07-15 19:06:34 +0700

    add files

---
+++ pincode/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  pincode
+//
+//  Created by chuyendo on 7/15/20.
+//  Copyright © 2020 DoThuyHanhChuyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+fsdf
